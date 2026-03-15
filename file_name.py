b016adc80a832522d0ee7d4ae37d5688209476a7
DemaFPS <dvorovds5@gmail.com>
2026-03-15 12:32:32 +0300

    Добавлено задание 6.2

--- file_name.py
+++ file_name.py
@@ -1,4 +1,14 @@
+#Задание 1
 s = "Hello, World!"
 print(s[0])
 print(s[-1])
 print(s[7:-1])
+
+#Задание 2
+#####
+str = input()
+if len(str) %2 == 0:
+    print(str.upper())
+else:
+    print(str.lower())
+#####
