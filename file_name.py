7907d8236412f64905eb984f201238ca2459137c
DemaFPS <dvorovds5@gmail.com>
2026-03-15 12:49:25 +0300

    Добавлено задание 6.5

--- file_name.py
+++ file_name.py
@@ -19,7 +19,7 @@ for i in str:
     if i in "aeiouAEIOU":
         count+=1
 print(count)
-"""
+
 #Задание 4
 str = input()
 rez=""
@@ -27,3 +27,17 @@ for i in range(len(str)):
     if i==0 or str[i] != str[i-1]:
         rez+=str[i]
 print(rez)
+"""
+#Задание 5
+str1=input()
+str2=input()
+c=0
+if len(str1) != len(str2):
+    print("False")
+for i in str1:
+    if i in str2:
+        c+=1
+if c!=0:
+    print("False")
+else:
+    print("True")
