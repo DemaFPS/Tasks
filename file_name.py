f4c0ce0a0baa50dcab74505be09f282b3915fdf0
DemaFPS <dvorovds5@gmail.com>
2026-03-15 12:37:41 +0300

    Добавлено задание 6.3

--- file_name.py
+++ file_name.py
@@ -5,10 +5,16 @@ print(s[-1])
 print(s[7:-1])
 
 #Задание 2
-#####
 str = input()
 if len(str) %2 == 0:
     print(str.upper())
 else:
     print(str.lower())
-#####
+
+#Задание 3
+str = input()
+count=0
+for i in str:
+    if i in "aeiouAEIOU":
+        count+=1
+print(count)
